cd34663a56877e5935fe30ccbb8d25d713cbbcad
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-02-09 03:32:45 +0100

    :rocket: | Deploy test base HTML.

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Page HTML de test</title>
+        <body>
+            <h2>Page de test</h2>
+            <p>
+                Cette page contient <strong>uniquement</strong> du code HTML.<br>
+                Voici quelques tests:
+            </p>
+            <ul>
+                <li style="color:blue;">Blue</li>
+                <li style="color:red;">Red</li>
+                <li style="color:green;">Green</li>
+            </ul>
+        </body>
+    </head>
+</html>
