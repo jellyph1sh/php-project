1c91c6b430f857d7a7c294625051c1be8052f5eb
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-02-09 04:09:07 +0100

    :white_check_mark: | Variable and conditions.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$userAge = 18;
+$canAccess = false;
+if ($userAge >= 18) {
+    $canAccess = true;
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,6 +12,16 @@
         <title>Page HTML de test</title>
     </head>
     <body>
+            <?php
+                if ($canAccess) {
+                    echo "<h1>Tu peux y accéder!</h1>";
+                } else {
+                    echo "<h1>Tu ne peux pas accéder!</h1>";
+                }
+            ?>
+            <?php if ($canAccess):?>
+                <h3><?php echo "Vous avez $userAge ans"?></h3>
+            <?php endif; ?>
             <h1>Bonjour, <?php echo "Je n'ai pas encore ton pseudo mais j'écris ça en PHP"?></h1>
             <h2>Vous avez commencé à consulter la page le <?php echo date("d/m/Y h:i:s");?></h2>
             <p>
