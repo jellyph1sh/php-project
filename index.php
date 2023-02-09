16d52b75ad57be0f167ec01e3a6ba7e6a9d4730f
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-02-09 03:43:29 +0100

    :sparkles: | Add consultation date.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
             <h1>Bonjour, <?php echo "Je n'ai pas encore ton pseudo mais j'écris ça en PHP"?></h1>
-            <h2>Page de test</h2>
+            <h2>Vous avez commencé à consulter la page le <?php echo date("d/m/Y h:i:s");?></h2>
             <p>
                 Cette page contient <strong>uniquement</strong> du code HTML.<br>
                 Voici quelques tests:
